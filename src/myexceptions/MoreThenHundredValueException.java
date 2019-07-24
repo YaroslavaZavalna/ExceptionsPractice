d15cfe7fd61e704ecc7faed2a4d018131b151119
yaroslava <zavalnaslava@gmail.com>
2019-07-24 21:24:20 +0300

    master: deleted unneeded files

--- src/myexceptions/MoreThenHundredValueException.java
+++
@@ -1,7 +0,0 @@
-package myexceptions;
-
-public class MoreThenHundredValueException extends Exception {
-    public MoreThenHundredValueException(){
-        super("Your input number is more than 100");
-    }
-}
